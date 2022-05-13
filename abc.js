8d21250976b77a43f396e169ba1ffcf53c4f2137
keniushadu <uou@foxmail.com>
2022-05-14 01:24:52 +0800

    Create abc.js

---
+++ abc.js
@@ -0,0 +1,5 @@
+if ($request.url.indexOf(atob("emZiamtieGN4emIueXFnei5iZWlqaW5nLmdvdi5jbi9hbGlwYXkvaGVhbHRoL2dldEhlYWx0aEluZm8=")) != -1) {
+        $done({status: 200, headers: $response.headers, body: '{"errCode":0,"errMsg":"请求成功","data":"Z4cF7BeWek98WYwY78nn47QZ0LS60h6K8VjHdMmloSxPhne5B8piWqJwtMiigw/Jpd4PCrGOQo1Uykc3jds3o0gZa/sUCAirBR+wLQ8mc5Uk2hC3rl/MTsV1/H5i74RY44By+hLaSkjGCuLIFRnfWpkthR8mSkdsQz7BRR+V+fgvcHlsgVdSaDY9tSvnzbwqq3ILHLXAaaaAxgzUIybM8aC7oXkLnMTo+Ai8x6suRmM="}' })
+}else{
+    $done({})
+}
